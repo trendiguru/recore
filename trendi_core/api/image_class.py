032c5f506935e7189e5e770424666fb608463ec8
Yonti Levin <yontilevin@gmail.com>
2017-02-13 18:05:50 +0200

    Update image_class.py
    added img type

--- trendi_core/api/image_class.py
+++ trendi_core/api/image_class.py
@@ -14,25 +14,33 @@ Class img_object(object):
     self.url = url
     self.page_url = page_url
     self.segmentation_method = method
+    self.type = url_sort(url)
     self.small_img_array = None
     self.img_hash = ''
     self.label = ''
-  
+
   def get_img_by_url(self):
     image = Utils.get_cv2_img_array(image_url)
-    if image is None:
-      return
-    small_img, _ = background_removal.standard_resize(image, 600)
-    self.small_img_array = small_img
+    if image is not None:
+        small_img, _ = background_removal.standard_resize(image, 600)
+        self.small_img_array = small_img
     
   def hash_img(self):
     if self.small_img_array is not None:
       self.img_hash = get_hash(self.small_img_array)
-  
+    
   def labelize(self):
     img_or_url = self.small_img_array or self.url
     self.label = labelize(img_or_url)
 
+    
+
+# Sorts image urls into "data", True (valid) or False (invalid)
+def url_sort(image_url):
+    if image_url[:4] == "data":
+        return 'data'
+    else:
+        return all(list(urlparse(image_url))[:3])
 
 def labelize(image_or_url):
     try:
