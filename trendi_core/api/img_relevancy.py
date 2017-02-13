67d04bc6098a62c10afca12e17a8f450c72a71dc
Yonti Levin <yontilevin@gmail.com>
2017-02-13 17:38:05 +0200

    Update img_relevancy.py

--- trendi_core/api/img_relevancy.py
+++ trendi_core/api/img_relevancy.py
@@ -18,45 +18,6 @@ start_synced_pipeline = Queue('start_synced_pipeline', connection=constants.redi
 add_results = Queue('add_results', connection=constants.redis_conn)
 LABEL_ADDRESS = "http://37.58.101.173:8357/neural/label"
 
-def check_if_exists(image_url, products):
-
-    # Temporarily remove whitelist for Recruit Test -- LS 22/06/2016.
-    # domain = tldextract.extract(page_url).registered_domain
-    # if not db.whitelist.find_one({'domain': domain}):
-    #     return False
-    # start = time.time()
-    if image_url[:4] == "data":
-        return False
-
-    def check_db(images_collection, products_collection):
-        image_obj = db[images_collection].find_one({'image_urls': image_url}, {'people.items.similar_results': 1})
-        if image_obj:
-            if products_collection in image_obj['people'][0]['items'][0]['similar_results'].keys():
-                return True
-            else:
-                add_results.enqueue_call(func=page_results.add_results_from_collection,
-                                         args=(image_obj['_id'], products_collection),
-                                         ttl=2000, result_ttl=2000, timeout=2000)
-                return False
-        else:
-            return False
-    if check_db('images', products):
-        return True
-    elif db.iip.find_one({'image_urls': image_url}):
-        return True
-    elif db.irrelevant_images.find_one({'image_urls': image_url}):
-        return False
-    else:
-        return None
-    # greens = {collection: Greenlet.spawn(check_db, collection, products) for collection in ['images', 'irrelevant_images', 'iip']}
-    # gevent.joinall(greens.values())
-    # if greens['images'].value or greens['iip'].value:
-    #     return True
-    # elif greens['irrelevant_images'].value:
-    #     return False
-    # print "after db checks: {0}".format(time.time()-start)
-    # return None
-
 
 def check_and_enqueue(image_url, page_url, products, method):
     image = Utils.get_cv2_img_array(image_url)
